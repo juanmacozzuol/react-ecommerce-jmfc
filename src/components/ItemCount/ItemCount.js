ab869ff3fb8da0a6d337750036a0bd298af062ee
Juanma Cozzuol <juanmacozzuol@gmail.com>
2022-02-08 12:04:07 -0300

    ItemCount Created

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,42 @@
+import {useState} from "react"
+
+const ItemCount = ({stock, initial, onAdd}) =>{
+
+    const [count, setCount]=useState(0);
+console.log();
+    const decrement =()=>{
+        if (count>initial)
+        {
+            setCount(count-1);
+        }
+
+ 
+
+    }
+
+    const increment =()=>{
+        if(count<stock){
+
+        
+        setCount(count+1);
+        }
+    }
+
+    return(
+        <div >
+        <button  onClick={decrement}>-</button>
+        <p style={{display:"inline"}}>{count}</p>
+        <button  onClick={increment}>+</button>
+        <div>
+        <button onClick={onAdd}>Agregar al carrito</button>
+        </div>
+        </div>
+    )
+    
+
+
+
+
+}
+
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,23 @@
 import "./ItemListContainer.css";
+import ItemCount from "../ItemCount/ItemCount";
 
+
+
+   
 
 const ItemListContainer =({title})=>{
 
+    const addItems =()=>{
+       console.log("items agregados");
+    }
+
 return(
 
-
+<div>
 <h2 class="itemlist">{title}</h2>
+<ItemCount stock="10" initial="0" onAdd={addItems} />
+
+</div>
 )
 
 
