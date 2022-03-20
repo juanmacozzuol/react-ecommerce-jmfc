3aa3fa2ff107bf3b29bffd28a46e500760cde8c0
Juanma Cozzuol <juanmacozzuol@gmail.com>
2022-03-20 15:50:07 -0300

    minor changes

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -1,13 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAH-82zP9qm5g0KAIWBX-UUws1LMo4IysI",
-//   authDomain: "e-commerce-boardgames.firebaseapp.com",
-//   projectId: "e-commerce-boardgames",
-//   storageBucket: "e-commerce-boardgames.appspot.com",
-//   messagingSenderId: "41591906450",
-//   appId: "1:41591906450:web:f31327b8a6707fe044064b"
-// };
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
